2319c5877fcd2a2bb5ab5b264a3908e542389a17
mokhkarabo <mokhkarabo>
2023-07-13 16:45:58 -0700

    put-c

---
+++ 0x00-hello_world/4-put.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Programming that prints a line with puts function
+ * Return: 0
+ */
+int main(void)
+{
+puts( "Programming is like building a multilingual puzzle");
+Return (0);
+}
