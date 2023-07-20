f0c53ea08f314ec27034932336a0a0f893aac0b3
mokhkarabo <mokhkarabo>
2023-07-20 06:34:25 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,28 +4,27 @@
  *
  * @n: the int to check
  *
- * Return: 0 and prints + if n is greater than zero
+ * Return: 1 and prints + if n is greater than zero
  *
- * 1 and prints 1 if n is zero
+ * 0 and prints 0 if n is zero
  *
  * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
-	if (n > 1)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 1)
+	else if (n == 0)
 	{
 		_putchar(48);
 		return (0);
 	}
-	else if (n < 1)
+	else if (n < 0)
 	{
 		_putchar('-');
 	}
-	return (0);
+	return (-1);
 }
-
