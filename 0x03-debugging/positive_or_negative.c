8f8cdcb9428e41e0802bf1faec002d48786379e1
mokhkarabo <mokhkarabo>
2023-07-20 23:11:23 -0700

    positive or negative

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,1 +1,17 @@
+include "main.h"
 
+void positive_or_negative(int num)
+{
+if (num > 0) 
+{
+printf("%d is positive.\n", num);
+}
+else if (num < 0)
+{
+printf("%d is negative.\n", num);
+}
+else
+{
+printf("%d is zero.\n", num);
+}
+}
